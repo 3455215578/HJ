fbc902b8b9986b509b6edfd5488b1fa5db4e703c
陈开烁 <3455215578@qq.com>
2025-01-11 07:37:07 +0800

    更新 vmc.c

--- wheel-legged-infantry-master/User/Algorithm/vmc/vmc.c
+++ wheel-legged-infantry-master/User/Algorithm/vmc/vmc.c
@@ -285,12 +285,12 @@ static void joint_motors_torque_set(Chassis *chassis,
 
 
         chassis->leg_L.vmc.forward_kinematics.Fxy_set_point.E.Fy_set_point =
-                95.0f
+                110.0f
                 + chassis->leg_L.leg_pos_pid.out
                 + chassis->chassis_roll_pid.out;
 
         chassis->leg_R.vmc.forward_kinematics.Fxy_set_point.E.Fy_set_point =
-                90.0f
+                100.0f
                 + chassis->leg_R.leg_pos_pid.out
                 - chassis->chassis_roll_pid.out;
 /***********************************************************************/
