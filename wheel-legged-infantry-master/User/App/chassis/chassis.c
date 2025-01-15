62c181232926a0d14e684ed1d95f38bd7f632689
陈开烁 <3455215578@qq.com>
2025-01-16 00:36:10 +0800

    这版腿长能用，但会震

--- wheel-legged-infantry-master/User/App/chassis/chassis.c
+++ wheel-legged-infantry-master/User/App/chassis/chassis.c
@@ -72,7 +72,7 @@ static void set_chassis_mode() {
             chassis.chassis_ctrl_info.height_m = 0.13f;
         }
         else if(switch_is_mid(get_rc_ctrl()->rc.s[RC_s_L])){
-            chassis.chassis_ctrl_info.height_m = 0.18f;
+            chassis.chassis_ctrl_info.height_m = 0.24f;
         }
         else if(switch_is_up(get_rc_ctrl()->rc.s[RC_s_L])){
             chassis.chassis_ctrl_info.height_m = 0.35f;
