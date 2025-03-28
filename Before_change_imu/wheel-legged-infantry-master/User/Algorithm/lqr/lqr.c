ee8e3b0e635c5bf39bf21bbd75497bfb3319f8d2
陈开烁 <3455215578@qq.com>
2025-03-28 21:29:50 +0800

    修改Imu解算之前的一版(离地检测不行)

---
+++ Before_change_imu/wheel-legged-infantry-master/User/Algorithm/lqr/lqr.c
@@ -0,0 +1,230 @@
+#include <stddef.h>
+#include <math.h>
+
+#include "lqr.h"
+#include "user_lib.h"
+#include "moving_filter.h"
+
+#include "robot_def.h"
+
+extern Chassis chassis;
+
+extern float vel_acc[2]; // 轮毂速度与加速度融合后的结果
+
+// 初始化K矩阵
+float wheel_K_L[6] = {0, 0, 0, 0, 0, 0};
+float joint_K_L[6] = {0, 0, 0, 0, 0, 0};
+
+float wheel_K_R[6] = {0, 0, 0, 0, 0, 0};
+float joint_K_R[6] = {0, 0, 0, 0, 0, 0};
+
+// K拟合系数矩阵
+float wheel_fitting_factor[6][4] = {
+        {-117.781292f,154.026270f,-97.775515f,-0.920719f},
+        {-2.816795f,3.215543f,-8.463449f,-0.263889f},
+
+        {-10.037614f,9.373294f,-2.846424f,-2.572780f},
+        {-7.527066f,6.884644f,-2.488566f,-3.875723f},
+
+        {-1930.586777f,2142.978232f,-901.638955f,144.530604f},
+        {-30.846395f,36.806359f,-18.028098f,5.976686f}
+};float joint_fitting_factor[6][4] = {
+        {257.193587f,-260.083448f,95.619582f,8.826665f},
+        {11.113783f,-7.818614f,-4.227715f,1.455345f},
+
+        {-91.405114f,100.562077f,-41.341926f,5.751901f},
+        {-140.887494f,152.608504f,-61.821895f,8.469409f},
+
+        {3618.052869f,-3484.499968f,1141.031351f,675.088502f},
+        {39.022983f,-47.427462f,25.607861f,13.046763f}
+};
+
+
+
+void chassis_K_matrix_fitting(float L0, float K[6], const float KL[6][4]) {
+  for (int i = 0; i < 6; i++) {
+    K[i] = KL[i][0] * powf(L0, 3) + KL[i][1] * powf(L0, 2) + KL[i][2] * powf(L0, 1) + KL[i][3] * powf(L0, 0);
+  }
+}
+
+// 计算状态变量theta
+static float cal_leg_theta(float phi0, float phi) {
+  float theta = 0, alpha = 0;//alpha is the Angle at which the virtual joint motor is turned
+  alpha = PI / 2 - phi0;
+
+  if (alpha * phi < 0) {
+    theta = ABS(alpha) - ABS(phi);
+    if ((alpha > 0) && (phi < 0)) {
+      theta *= -1;
+    } else {
+
+    }
+  } else {
+    theta = ABS(alpha) + ABS(phi);
+    if ((alpha < 0) && (phi < 0)) {
+    } else {
+      theta *= -1;
+    }
+  }
+  return theta;
+}
+
+static void state_variable_update(Leg* leg_L, Leg* leg_R, float phi, float phi_dot) {
+  if ((leg_L == NULL) || (leg_R == NULL)) {
+    return;
+  }
+  //theta_last
+  leg_L->state_variable_feedback.theta_last = leg_L->state_variable_feedback.theta;
+  leg_R->state_variable_feedback.theta_last = leg_R->state_variable_feedback.theta;
+
+  //1.theta
+  leg_L->state_variable_feedback.theta = cal_leg_theta(leg_L->vmc.forward_kinematics.fk_phi.phi0, phi);
+  leg_R->state_variable_feedback.theta = cal_leg_theta(leg_R->vmc.forward_kinematics.fk_phi.phi0, phi);
+
+  chassis.theta_error = leg_L->state_variable_feedback.theta - leg_R->state_variable_feedback.theta;
+
+  //2.theta_dot、theta_ddot
+  leg_L->state_variable_feedback.theta_dot_last = leg_L->state_variable_feedback.theta_dot;
+  leg_L->state_variable_feedback.theta_dot = (leg_L->state_variable_feedback.theta - leg_L->state_variable_feedback.theta_last) / (CHASSIS_PERIOD * 0.001f);
+  float theta_ddot_raw_L = (leg_L->state_variable_feedback.theta_dot - leg_L->state_variable_feedback.theta_dot_last) / (CHASSIS_PERIOD * 0.001f);
+  update_moving_average_filter(&leg_L->theta_ddot_filter, theta_ddot_raw_L);
+  leg_L->state_variable_feedback.theta_ddot = get_moving_average_filtered_value(&leg_L->theta_ddot_filter);
+
+  leg_R->state_variable_feedback.theta_dot_last = leg_R->state_variable_feedback.theta_dot;
+  leg_R->state_variable_feedback.theta_dot = (leg_R->state_variable_feedback.theta - leg_R->state_variable_feedback.theta_last) / (CHASSIS_PERIOD * 0.001f);
+  float theta_ddot_raw_R = (leg_R->state_variable_feedback.theta_dot - leg_R->state_variable_feedback.theta_dot_last) / (CHASSIS_PERIOD * 0.001f);
+  update_moving_average_filter(&leg_R->theta_ddot_filter, theta_ddot_raw_R);
+  leg_R->state_variable_feedback.theta_ddot = get_moving_average_filtered_value(&leg_R->theta_ddot_filter);
+
+  //4.x_dot
+  leg_L->state_variable_feedback.x_dot = vel_acc[0];
+  leg_R->state_variable_feedback.x_dot = vel_acc[0];
+
+  //3.x
+
+  if(chassis.chassis_ctrl_info.v_m_per_s != 0.0f)
+  {
+      leg_L->state_variable_feedback.x = 0.0f;
+      leg_R->state_variable_feedback.x = 0.0f;
+  }
+  else
+  {
+      leg_L->state_variable_feedback.x = leg_L->state_variable_feedback.x + CHASSIS_PERIOD * 0.001f * leg_L->state_variable_feedback.x_dot;
+      leg_R->state_variable_feedback.x = leg_R->state_variable_feedback.x + CHASSIS_PERIOD * 0.001f * leg_R->state_variable_feedback.x_dot;
+
+  }
+
+  // x_ddot
+  leg_L->state_variable_feedback.x_dot_last = leg_L->state_variable_feedback.x_dot;
+  leg_L->state_variable_feedback.x_ddot = (leg_L->state_variable_feedback.x_dot - leg_L->state_variable_feedback.x_dot_last) / (CHASSIS_PERIOD * 0.001f);
+  leg_R->state_variable_feedback.x_dot_last = leg_R->state_variable_feedback.x_dot;
+  leg_R->state_variable_feedback.x_ddot = (leg_R->state_variable_feedback.x_dot - leg_R->state_variable_feedback.x_dot_last) / (CHASSIS_PERIOD * 0.001f);
+
+  //5.phi
+  leg_L->state_variable_feedback.phi = phi;
+  leg_R->state_variable_feedback.phi = phi;
+
+  // 6.phi_dot
+  leg_L->state_variable_feedback.phi_dot = phi_dot;
+  leg_R->state_variable_feedback.phi_dot = phi_dot;
+}
+
+static void state_variable_set(Leg* leg_L, Leg* leg_R) {
+
+  leg_L->state_variable_set_point.x = 0.0f;
+  leg_L->state_variable_set_point.x_dot = chassis.chassis_ctrl_info.v_m_per_s;
+  leg_L->state_variable_set_point.theta = 0.0f;
+  leg_L->state_variable_set_point.theta_dot = 0.0f;
+  leg_L->state_variable_set_point.phi = 0.0f;
+  leg_L->state_variable_set_point.phi_dot = 0.0f;
+
+  leg_R->state_variable_set_point.x = 0.0f;
+  leg_R->state_variable_set_point.x_dot = chassis.chassis_ctrl_info.v_m_per_s;
+  leg_R->state_variable_set_point.theta = 0.0f;
+  leg_R->state_variable_set_point.theta_dot = 0.0f;
+  leg_R->state_variable_set_point.phi = 0.0f;
+  leg_R->state_variable_set_point.phi_dot = 0.0f;
+
+}
+
+static void state_variable_error(Leg *leg_L, Leg *leg_R) {
+  if (leg_L == NULL || leg_R == NULL) {
+    return;
+  }
+
+  leg_L->state_variable_error.x = leg_L->state_variable_feedback.x - leg_L->state_variable_set_point.x;
+  leg_L->state_variable_error.x_dot = leg_L->state_variable_feedback.x_dot - leg_L->state_variable_set_point.x_dot;
+  leg_L->state_variable_error.theta = leg_L->state_variable_feedback.theta - leg_L->state_variable_set_point.theta;
+  leg_L->state_variable_error.theta_dot = leg_L->state_variable_feedback.theta_dot - leg_L->state_variable_set_point.theta_dot;
+  leg_L->state_variable_error.phi = leg_L->state_variable_feedback.phi - leg_L->state_variable_set_point.phi;
+  leg_L->state_variable_error.phi_dot = leg_L->state_variable_feedback.phi_dot - leg_L->state_variable_set_point.phi_dot;
+
+
+  leg_R->state_variable_error.x = leg_R->state_variable_feedback.x - leg_R->state_variable_set_point.x;
+  leg_R->state_variable_error.x_dot = leg_R->state_variable_feedback.x_dot - leg_R->state_variable_set_point.x_dot;
+  leg_R->state_variable_error.theta = leg_R->state_variable_feedback.theta - leg_R->state_variable_set_point.theta;
+  leg_R->state_variable_error.theta_dot = leg_R->state_variable_feedback.theta_dot - leg_R->state_variable_set_point.theta_dot;
+  leg_R->state_variable_error.phi = leg_R->state_variable_feedback.phi - leg_R->state_variable_set_point.phi;
+  leg_R->state_variable_error.phi_dot = leg_R->state_variable_feedback.phi_dot - leg_R->state_variable_set_point.phi_dot;
+
+}
+
+static void state_variable_out(Leg* leg_L, Leg* leg_R) {
+
+  leg_L->state_variable_wheel_out.theta = leg_L->state_variable_error.theta * wheel_K_L[0]; // 极性正确
+  leg_L->state_variable_wheel_out.theta_dot = leg_L->state_variable_error.theta_dot * wheel_K_L[1]; // 极性正确
+  leg_L->state_variable_wheel_out.x = leg_L->state_variable_error.x * wheel_K_L[2];
+  leg_L->state_variable_wheel_out.x_dot = leg_L->state_variable_error.x_dot * wheel_K_L[3];
+  leg_L->state_variable_wheel_out.phi = leg_L->state_variable_error.phi * wheel_K_L[4]; // 极性正确
+  leg_L->state_variable_wheel_out.phi_dot = leg_L->state_variable_error.phi_dot * wheel_K_L[5]; // 极性正确
+
+  leg_R->state_variable_wheel_out.theta = leg_R->state_variable_error.theta * wheel_K_R[0]; // 极性正确
+  leg_R->state_variable_wheel_out.theta_dot = leg_R->state_variable_error.theta_dot * wheel_K_R[1]; // 极性正确
+  leg_R->state_variable_wheel_out.x = leg_R->state_variable_error.x * wheel_K_R[2];
+  leg_R->state_variable_wheel_out.x_dot = leg_R->state_variable_error.x_dot * wheel_K_R[3];
+  leg_R->state_variable_wheel_out.phi = leg_R->state_variable_error.phi * wheel_K_R[4]; // 极性正确
+  leg_R->state_variable_wheel_out.phi_dot = leg_R->state_variable_error.phi_dot * wheel_K_R[5]; // 极性正确
+
+  leg_L->state_variable_joint_out.theta = leg_L->state_variable_error.theta * joint_K_L[0];
+  leg_L->state_variable_joint_out.theta_dot = leg_L->state_variable_error.theta_dot * joint_K_L[1];
+  leg_L->state_variable_joint_out.x = leg_L->state_variable_error.x * joint_K_L[2];
+  leg_L->state_variable_joint_out.x_dot = leg_L->state_variable_error.x_dot * joint_K_L[3];
+  leg_L->state_variable_joint_out.phi = leg_L->state_variable_error.phi * joint_K_L[4];
+  leg_L->state_variable_joint_out.phi_dot = leg_L->state_variable_error.phi_dot * joint_K_L[5];
+
+  leg_R->state_variable_joint_out.theta = leg_R->state_variable_error.theta * joint_K_R[0];
+  leg_R->state_variable_joint_out.theta_dot = leg_R->state_variable_error.theta_dot * joint_K_R[1];
+  leg_R->state_variable_joint_out.x = leg_R->state_variable_error.x * joint_K_R[2];
+  leg_R->state_variable_joint_out.x_dot = leg_R->state_variable_error.x_dot * joint_K_R[3];
+  leg_R->state_variable_joint_out.phi = leg_R->state_variable_error.phi * joint_K_R[4];
+  leg_R->state_variable_joint_out.phi_dot = leg_R->state_variable_error.phi_dot * joint_K_R[5];
+
+}
+
+/*******************************************************************************
+ *                                     LQR                                     *
+ *******************************************************************************/
+
+void lqr_ctrl(void) {
+
+  // K矩阵拟合
+  chassis_K_matrix_fitting(chassis.leg_L.vmc.forward_kinematics.fk_L0.L0, wheel_K_L, wheel_fitting_factor);
+  chassis_K_matrix_fitting(chassis.leg_L.vmc.forward_kinematics.fk_L0.L0, joint_K_L, joint_fitting_factor);
+  chassis_K_matrix_fitting(chassis.leg_R.vmc.forward_kinematics.fk_L0.L0, wheel_K_R, wheel_fitting_factor);
+  chassis_K_matrix_fitting(chassis.leg_R.vmc.forward_kinematics.fk_L0.L0, joint_K_R, joint_fitting_factor);
+
+  // 更新反馈状态变量
+  state_variable_update(&chassis.leg_L,
+                        &chassis.leg_R,
+                        chassis.imu_reference.pitch_angle,
+                        chassis.imu_reference.pitch_gyro);
+
+  // 设置期望状态变量
+  state_variable_set(&chassis.leg_L, &chassis.leg_R);
+
+  // 计算误差(反馈 - 期望)
+  state_variable_error(&chassis.leg_L, &chassis.leg_R);
+
+  // 计算每个状态变量项的输出(注意极性)
+  state_variable_out(&chassis.leg_L, &chassis.leg_R);
+}
