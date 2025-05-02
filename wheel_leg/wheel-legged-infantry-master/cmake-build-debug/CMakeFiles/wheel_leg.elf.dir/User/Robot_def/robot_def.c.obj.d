9d48257f62e0f81a788ec57f8b64a6f71a8a079e
陈开烁 <3455215578@qq.com>
2025-05-02 18:03:33 +0800

    封装了CAN发送和接收，关节CAN1,轮毂CAN2

--- wheel_leg/wheel-legged-infantry-master/cmake-build-debug/CMakeFiles/wheel_leg.elf.dir/User/Robot_def/robot_def.c.obj.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/wheel_leg.elf.dir/User/Robot_def/robot_def.c.obj: \
- D:\GitHub\Code\HJ\wheel_leg\wheel-legged-infantry-master\User\Robot_def\robot_def.c \
- D:\GitHub\Code\HJ\wheel_leg\wheel-legged-infantry-master\User\Robot_def\robot_def.h \
- d:\jetbrains\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
- D:\GitHub\Code\HJ\wheel_leg\wheel-legged-infantry-master\User\Algorithm\pid/pid.h \
- d:\jetbrains\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdint-gcc.h \
- D:\GitHub\Code\HJ\wheel_leg\wheel-legged-infantry-master\User\Algorithm\filter\moving_filter/moving_filter.h
